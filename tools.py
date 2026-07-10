a1e17c76d85df64817494deeb1322eeeb2df6d6f
toldom12 <michal.gadzinski1@gmail.com>
2026-07-10 17:11:34 +0200

    add extended command

--- tools.py
+++ tools.py
@@ -1,4 +1,5 @@
 import struct
+import time
 from enum import StrEnum, IntEnum
 
 import serial
@@ -9,6 +10,10 @@ class Commands(IntEnum):
     ReadPosition = 0x54
     ReadTurnsCounter = 0x55
     ExtendedCommands = 0x56
+
+class ExtendedCommands(IntEnum):
+    SetZeroPosition = 0x5E
+    ResetEncoder = 0x75
 
 
 class EncoderHandling:
@@ -30,7 +35,7 @@ class EncoderHandling:
         self.serial.reset_input_buffer()
 
     def execute_command_encoder(self,
-                                cmd: Commands):
+                                cmd: Commands) -> int:
         convert_data = bytes([cmd])
         self.serial.write(convert_data)
 
@@ -45,15 +50,37 @@ class EncoderHandling:
 
         return position
 
+    def execute_extended_commands(self,
+                                  cmd: ExtendedCommands,
+                                  wait_before : int | float = 0.1,
+                                  wait_after: int | float = 0.1) -> None:
+
+        extended_command= bytes([Commands.ExtendedCommands, cmd])
+
+        time.sleep(wait_before)
+        self.serial.write(extended_command)
+        self.serial.flush()
+        time.sleep(wait_after)
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
     e = EncoderHandling(port=com_port,
                         parity=serial.PARITY_NONE,
                         boundrate=boundrate)
-    pos1 = e.execute_command_encoder(cmd = Commands.ReadPosition)
+    pos1 = e.execute_command_encoder(cmd = Commands.ReadTurnsCounter)
 
-    pos2 = e.execute_command_encoder(cmd = Commands.ReadPosition)
+    pos2 = e.execute_command_encoder(cmd = Commands.ReadTurnsCounter)
     pass
 
 
