02189b145f09282ee2be761ce1d504241337c134
toldom12 <michal.gadzinski1@gmail.com>
2026-08-13 11:52:48 +0200

    add: automated tests for encoder

--- automated_tests/encoder/test_encoder.py
+++ automated_tests/encoder/test_encoder.py
@@ -1,16 +1,26 @@
 import pytest
 
 from automated_tests.encoder.map import StartUp
-from automated_tests.encoder.tools_encoder import CheckCommunicationEnocderCase, EncoderInfo
+from automated_tests.encoder.tools_encoder import CheckCommunicationEnocderCase, EncoderInfo, EncoderPlatform
+from tools.StepsVerifcator.steps_verifcator import TesStepsVerifcator
 
 
-@pytest.mark.parametrize('data_parameters', [
+@pytest.mark.parametrize('parameters_encoder', [
        params.value for params in CheckCommunicationEnocderCase])
 def test_check_communication_encoder(module_data: StartUp,
-                                     data_parameters : EncoderInfo):
+                                     parameters_encoder : EncoderInfo):
 
-      info = module_data.encoder
-      pass
+
+      test = TesStepsVerifcator()
+
+      EncoderPlatform.check_encoder_connection(encoder_init_params=module_data.encoder,
+                                               expected_parameters_encoder=parameters_encoder,
+                                               test = test)
+
+
+
+      assert test.received_results == test.expected_results
+
 
 
 
--- automated_tests/encoder/tools_encoder.py
+++ automated_tests/encoder/tools_encoder.py
@@ -2,6 +2,10 @@ from dataclasses import dataclass
 from enum import StrEnum, Enum
 from typing import Optional
 
+from automated_tests.encoder.map import StartUp
+from tools.StepsVerifcator.steps_verifcator import TesStepsVerifcator
+from tools.encoder.tools_encoder_handling import EncoderHandling
+
 
 @dataclass
 class EncoderInfo:
@@ -17,3 +21,31 @@ class CheckCommunicationEnocderCase(Enum):
                          boundrate=115200,
                          stepper_motor_steps=200)
 
+
+
+class EncoderPlatform:
+    @staticmethod
+    def check_encoder_connection(encoder_init_params: EncoderHandling,
+                           expected_parameters_encoder :EncoderInfo,
+                           test: TesStepsVerifcator) -> TesStepsVerifcator:
+
+        test.add_test(condition=bool(encoder_init_params.port == expected_parameters_encoder.com_port),
+                      fail_msg=f'[ID_0] Not correctly port COM, '
+                               f'encoder_received: {encoder_init_params.port},'
+                               f'encoder_expected: {expected_parameters_encoder.com_port},'
+                               f'')
+
+        test.add_test(condition=bool(encoder_init_params.boundrate == expected_parameters_encoder.boundrate),
+                      fail_msg=f'[ID_1] Not correctly boundrate , '
+                               f'encoder_received: {encoder_init_params.boundrate},'
+                               f'encoder_expected: {expected_parameters_encoder.boundrate},'
+                               f'')
+
+        test.add_test(condition=bool(encoder_init_params.stepper_steps_per_rotation ==
+                                     expected_parameters_encoder.stepper_motor_steps),
+                      fail_msg=f'Not stepper motor steppes '
+                               f'encoder_received: {encoder_init_params.stepper_steps_per_rotation},'
+                               f'encoder_expected: {expected_parameters_encoder.stepper_motor_steps},'
+                               f'')
+
+        return test
