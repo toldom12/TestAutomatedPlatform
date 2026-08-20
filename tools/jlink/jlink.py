be8a01202d628b734532f4cd0eaed3f683d1cef3
toldom12 <michal.gadzinski1@gmail.com>
2026-08-20 10:29:31 +0200

    fixes jlink erase

--- tools/jlink/jlink.py
+++ tools/jlink/jlink.py
@@ -3,16 +3,18 @@ import subprocess
 from pathlib import Path
 
 from setup import configurator
+from setup.configurator import jlink_path
 
 DIR_PATH = Path(__file__).resolve().parent / 'tmp_files'
 
 def build_command_jlink(parameters_list : list) -> str:
-    path_to_jlink_file = f'{DIR_PATH}\\tmp.jlink'
-    with open(rf'{path_to_jlink_file}', 'w') as f:
+    DIR_PATH.mkdir(parents=True, exist_ok=True)
+    path_to_jlink_file = DIR_PATH / 'tmp.jlink'
+    with path_to_jlink_file.open('w') as f:
         for param in parameters_list:
             f.write(param + '\n')
 
-    return path_to_jlink_file
+    return str(path_to_jlink_file)
 
 
 #@TODO : add PID handling
@@ -20,16 +22,16 @@ def build_command_jlink(parameters_list : list) -> str:
 class Jlink:
 
     jlink_path = configurator.jlink_path
-    
+
     @staticmethod
-    def flash(jlink_path: str,
+    def flash(
               processor: str,
               jlink_sn: int,
               hex_file: str):
 
         output = None
         jlink_command_file = build_command_jlink(parameters_list=[
-            'connect'
+            'connect',
             'erase',
              f'loadfile {hex_file}',
              'r',
@@ -39,10 +41,10 @@ class Jlink:
             jlink_path,
             "-SelectEmuBySn", str(jlink_sn),
             "-Device", processor,
-            "-If", "SWD"
-            "-Speed", "4000"
+            "-If", "SWD",
+            "-Speed", "4000",
             "-AutoConnect", "1",
-            "-ExitOnError", "1"
+            "-ExitOnError", "1",
             "-CommandFile", rf"{jlink_command_file}",
         ]
 
@@ -54,18 +56,18 @@ class Jlink:
                 capture_output=True,
                 check=True,
             )
-        except Exception as error:
+        except subprocess.CalledProcessError as error:
             print(f'[ERROR][JFlash]: flash {error},'
-                  f'ouptut: {output.stdout}')
+                  f'output: {error.stdout}\n{error.stderr}')
 
     @staticmethod
-    def erase(jlink_path: str,
-              processor: str,
+    def erase(processor: str,
               jlink_sn: int):
 
         output = None
         jlink_command_file = build_command_jlink(parameters_list=[
             'connect',
+            'h',
             'erase',
             'exit'
         ])
@@ -74,11 +76,11 @@ class Jlink:
             jlink_path,
             "-SelectEmuBySn", str(jlink_sn),
             "-Device", processor,
-            "-If", "SWD"
-             "-Speed", "4000"
+            "-If", "SWD",
+            "-Speed", "4000",
             "-AutoConnect", "1",
-            "-ExitOnError", "1"
-                            "-CommandFile", rf"{jlink_command_file}",
+            "-ExitOnError", "1",
+            "-CommandFile", rf"{jlink_command_file}",
         ]
 
         try:
@@ -88,20 +90,15 @@ class Jlink:
                 capture_output=True,
                 check=True,
             )
-        except Exception as error:
+        except subprocess.CalledProcessError as error:
             print(f'[ERROR][JFlash] erase: {error},'
-                  f'output: {output.stdout}')
+                  f'output: {error.stdout}\n{error.stderr}')
 
 
 
 if __name__ == '__main__':
-    # load_file = 'C:\\Repos\\AutomatedTestsEmbeddedPlatform\\artifacts\\arduino\\nano\\flash\\flash.ino'
-    # z = build_command_jlink(parameters_list=['erase',
-    #                                      f'loadfile {load_file}',
-    #                                      'r',
-    #                                      'q'
-    #                                          ])
-
-    pass
+   Jlink.erase(processor=configurator.board_1_uc,
+               jlink_sn=configurator.board_1_sn)
+   pass
 
 
