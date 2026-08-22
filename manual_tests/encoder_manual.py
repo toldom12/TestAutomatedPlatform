1e731f4f1b5c19b85dfc8bac1664dafdd565f766
toldom12 <michal.gadzinski1@gmail.com>
2026-08-22 10:58:00 +0200

    add cfg json

--- manual_tests/encoder_manual.py
+++ manual_tests/encoder_manual.py
@@ -1,11 +1,25 @@
-from automated_tests.encoder.conftest import module_data
+from pathlib import Path
+
 from automated_tests.encoder.map import StartUp
 from tools.encoder.tools_encoder_handling import Commands
+from tools.encoder.tools_encoder_handling import EncoderHandling
+from setup.configurator import com_port, boundrate
+from serial.serialutil import PARITY_NONE
+from serial.serialutil import SerialException
 
-initialize_module = module_data()
+try:
+    initialize_module = StartUp(encoder=EncoderHandling(port=com_port,
+                                                      parity=PARITY_NONE,
+                                                      boundrate=boundrate))
+except SerialException as e:
+    print(f"Could not open serial port '{com_port}': {e}")
+    raise SystemExit(1)
 
 def check_position(initialize_mode: StartUp):
-    pos1 = initialize_mode.encoder.execute_measurement_in_percentage(cmd=Commands.ReadPosition,
-                                               valve_steps=200)
+    pos1 = initialize_mode.encoder.execute_measurement_in_percentage(
+        cmd=Commands.ReadPosition,
+        valve_steps=200,
+    )
 
-    pass
+if __name__ == '__main__':
+    check_position(initialize_mode=initialize_module)
